4b0cfbdcacc8acfafeabfeac1ae522d37477e140
facm404 <136346659+facm404@users.noreply.github.com>
2024-02-22 00:47:00 -0300

    Add: onFailure and try catch

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -10,30 +10,35 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private val urlBase= "https://jsonplaceholder.typicode.com/"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         val retrofit= Retrofit.Builder()
             .baseUrl(urlBase)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val service = retrofit.create(PostApiService::class.java)
+
         lifecycleScope.launch {
-            val response = service.getUserPost()
-            response.forEach{
-                println(it)
-            }
-            val titlesAndIds = response.map { "ID: ${it.id}, Title: ${it.title}" }
-            val allTitlesAndIds = titlesAndIds.joinToString("\n")
-            runOnUiThread {
-                val textView = findViewById<TextView>(R.id.tv_title)
-                textView.text = allTitlesAndIds
+            try {
+                val response = service.getUserPost()
+
+                val titlesAndIds = response.map { "ID: ${it.id}, Title: ${it.title}" }
+                val allTitlesAndIds = titlesAndIds.joinToString("\n")
+
+                runOnUiThread {
+                    val textView = findViewById<TextView>(R.id.tv_title)
+                    textView.text = allTitlesAndIds
+                }
+            } catch (e: Exception) {
+
+                runOnUiThread {
+                    val textView = findViewById<TextView>(R.id.tv_title)
+                    textView.text = "Error: ${e.message}"
+                }
             }
         }
     }
-
-
-
 }
